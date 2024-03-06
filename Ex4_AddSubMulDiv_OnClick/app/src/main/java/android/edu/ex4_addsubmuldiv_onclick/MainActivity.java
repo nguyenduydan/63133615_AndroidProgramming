363a0d5addfacd39f59747f3d24da87ba7c03248
FOG <dn135897@gmail.com>
2024-03-06 15:10:24 +0700

    Update Ex4

--- Ex4_AddSubMulDiv_OnClick/app/src/main/java/android/edu/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_OnClick/app/src/main/java/android/edu/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -4,6 +4,7 @@ import static java.lang.Integer.parseInt;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -13,21 +14,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //khai báo cá đối tượng
+    EditText valueA;
+    EditText valueB;
+    EditText Kq;
+    Button btnAdd,btnSub,btnMul,btnDiv;
+    void TimDieuKhien(){
+        valueA = (EditText) findViewById(R.id.edt_a);
+        valueB = (EditText) findViewById(R.id.edt_b);
+        Kq = (EditText) findViewById(R.id.edtkq);
+        btnAdd = (Button) findViewById(R.id.btnCong);
+        btnSub = (Button) findViewById(R.id.btnTru);
+        btnMul = (Button) findViewById(R.id.btnNhan);
+        btnDiv = (Button) findViewById(R.id.btnChia);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
     }
+
+
 
     //Hàm xử lý cộng
     public void XuLyCong(View view){
-        //lấy tham số
-        EditText valueA = (EditText) findViewById(R.id.edt_a);
-        EditText valueB = (EditText) findViewById(R.id.edt_b);
-        EditText Kq = (EditText) findViewById(R.id.edtkq);
-
         double numA = Double.parseDouble(valueA.getText().toString());
         double numB = Double.parseDouble(valueB.getText().toString());
         double result;
@@ -37,10 +50,6 @@ public class MainActivity extends AppCompatActivity {
     }
     //Hàm xử lý trừ
     public void XuLyTru(View view){
-        EditText valueA = (EditText) findViewById(R.id.edt_a);
-        EditText valueB = (EditText) findViewById(R.id.edt_b);
-        EditText Kq = (EditText) findViewById(R.id.edtkq);
-
         double numA = Double.parseDouble(valueA.getText().toString());
         double numB = Double.parseDouble(valueB.getText().toString());
         double result;
@@ -50,10 +59,6 @@ public class MainActivity extends AppCompatActivity {
     }
     //Hàm xử lý nhân
     public void XuLyNhan(View view){
-        EditText valueA = (EditText) findViewById(R.id.edt_a);
-        EditText valueB = (EditText) findViewById(R.id.edt_b);
-        EditText Kq = (EditText) findViewById(R.id.edtkq);
-
         double numA = Double.parseDouble(valueA.getText().toString());
         double numB = Double.parseDouble(valueB.getText().toString());
         double result;
@@ -63,10 +68,6 @@ public class MainActivity extends AppCompatActivity {
     }
     //Hàm xử lý chia
     public void XuLyChia(View view){
-        EditText valueA = (EditText) findViewById(R.id.edt_a);
-        EditText valueB = (EditText) findViewById(R.id.edt_b);
-        EditText Kq = (EditText) findViewById(R.id.edtkq);
-
         double numA = Double.parseDouble(valueA.getText().toString());
         double numB = Double.parseDouble(valueB.getText().toString());
         double result;
