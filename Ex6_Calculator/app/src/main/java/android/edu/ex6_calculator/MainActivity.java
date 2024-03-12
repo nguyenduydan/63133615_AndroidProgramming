d5c448138018278045c0869ab06aa97918c1abb8
FOG <dn135897@gmail.com>
2024-03-12 07:49:33 +0700

    Create Interface BMI Calculator

--- Ex6_Calculator/app/src/main/java/android/edu/ex6_calculator/MainActivity.java
+++ Ex6_Calculator/app/src/main/java/android/edu/ex6_calculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
