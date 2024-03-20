b0841f67eaacc7d0e25ad1dc58ba715062150626
FOG <dn135897@gmail.com>
2024-03-20 14:09:45 +0700

    Thêm phần Intent chuyển màn hình

---
+++ Login_App/app/src/main/java/android/edu/login_app/LoginActivity.java
@@ -0,0 +1,51 @@
+package android.edu.login_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText txt_user, txt_pass;
+    Button login,register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        txt_user = (EditText) findViewById(R.id.username);
+        txt_pass = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.btn_login);
+        register = (Button) findViewById(R.id.btn_signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt_user.getText().toString().equals("admin") && txt_pass.getText().toString().equals("admin")) {
+                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(LoginActivity.this, "Đăng nhập không thành công!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        chuyenManHinh();
+    }
+
+    public void chuyenManHinh(){
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
