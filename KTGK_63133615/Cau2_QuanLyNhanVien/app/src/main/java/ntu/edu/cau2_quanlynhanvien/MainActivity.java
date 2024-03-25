a0b3f7fc63c4a96377b191d81e23d356e9682563
FOG <dn135897@gmail.com>
2024-03-25 15:03:55 +0700

    Hoàn thành giao diện

--- KTGK_63133615/Cau2_QuanLyNhanVien/app/src/main/java/ntu/edu/cau2_quanlynhanvien/MainActivity.java
+++ KTGK_63133615/Cau2_QuanLyNhanVien/app/src/main/java/ntu/edu/cau2_quanlynhanvien/MainActivity.java
@@ -1,9 +1,15 @@
 package ntu.edu.cau2_quanlynhanvien;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SharedMemory;
+import android.view.View;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
 
     }
 }
