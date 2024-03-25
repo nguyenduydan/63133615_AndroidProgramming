4be01a68c2b503c2b7e50429e07684a5be16c5d6
FOG <dn135897@gmail.com>
2024-03-25 10:10:12 +0700

    Tạo project Cau2 và thiết kế giao diện Login

---
+++ KTGK_63133615/Cau2_QuanLyProject/app/src/main/java/ntu/edu/cau2_quanlyproject/Login.java
@@ -0,0 +1,17 @@
+package ntu.edu.cau2_quanlyproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+    }
+}
