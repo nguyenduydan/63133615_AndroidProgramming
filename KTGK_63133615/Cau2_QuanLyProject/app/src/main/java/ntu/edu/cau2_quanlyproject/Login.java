386c27e954266c0f800c0fa45ab0f7c20f66d690
FOG <dn135897@gmail.com>
2024-03-25 10:20:50 +0700

    Xử lý bấm nút login

--- KTGK_63133615/Cau2_QuanLyProject/app/src/main/java/ntu/edu/cau2_quanlyproject/Login.java
+++ KTGK_63133615/Cau2_QuanLyProject/app/src/main/java/ntu/edu/cau2_quanlyproject/Login.java
@@ -1,11 +1,23 @@
 package ntu.edu.cau2_quanlyproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    Button btn_login;
+    EditText edtName,edtPwd;
+
+    void FindId(){
+        btn_login = (Button) findViewById(R.id.btn_login);
+        edtName = (EditText) findViewById(R.id.edt_username);
+        edtPwd = (EditText) findViewById(R.id.edt_pwd);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,22 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        FindId();
+        Login();
     }
+
+    public void Login(){
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtName.getText().toString().equals("admin") && edtPwd.getText().toString().equals("admin")){
+                    Toast.makeText(Login.this, "Đăng nhập thành công!", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(Login.this, "Đăng nhập không thành công!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
