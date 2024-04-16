a4618f7ca11b4b76627783e37665bf51d8eae810
FOG <dn135897@gmail.com>
2024-04-16 09:14:37 +0700

    BT_RecyclerView ViewPaper2

---
+++ Bt_RecyclerViewViewPaper2/app/src/main/java/ntu/edu/bt_recyclerviewviewpaper2/LandScapeAdapter.java
@@ -0,0 +1,87 @@
+package ntu.edu.bt_recyclerviewviewpaper2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land,parent,false);
+        ItemLandHolder viewHolderCreated =  new ItemLandHolder(vItem);
+        return viewHolderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        //Lấy đối tượng hiển thị
+        LandScape landScapeShow =  lstData.get(position);
+        // Trích thông tin
+        String caption = landScapeShow.getLandscapeCaption();
+        String tenFileAnh = landScapeShow.getLandscapeImg();
+        //Đặt vào các trường thông tin của holder
+        holder.tvCaption.setText(caption);
+            //Đặt ảnh
+            String packageName = holder.itemView.getContext().getPackageName();
+            int imgId = holder.itemView.getResources().getIdentifier(tenFileAnh,"mipmap",packageName);
+        holder.ivLandScape.setImageResource(imgId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstData.size();
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView tvCaption;
+        ImageView ivLandScape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCaption);
+            ivLandScape = itemView.findViewById(R.id.img_land);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int viTriOnClick = getAdapterPosition();
+            LandScape ptDuocClick = lstData.get(viTriOnClick);
+            //Bóc thông tin
+            String ten = ptDuocClick.getLandscapeCaption();
+            String tenFile = ptDuocClick.getLandscapeImg();
+            //Toast ten
+            String chuoiThongBao="Bạn vừa click vào " + ten;
+            Toast.makeText(v.getContext(),chuoiThongBao,Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Bt_RecyclerViewViewPaper2/app/src/main/java/ntu/edu/bt_recyclerviewviewpaper2/MainActivity.java
@@ -0,0 +1,45 @@
+package ntu.edu.bt_recyclerviewviewpaper2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //3.Chuẩn bị dữ liệu cho landScapeList
+        recyclerViewDatas = getDataForRecyclerView();
+        //4. Tìm điều khiển Recycler
+        recyclerView = findViewById(R.id.recyclerLand);
+        //5. Tạo layout manager để đặt bố cục cho Recycler
+        RecyclerView.LayoutManager layoutLinearHorizontal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        //6. Tạo adapter gắn với nguồn dữ liệu
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //7. Gắn adapter với Recycler
+        recyclerView.setAdapter(landScapeAdapter);
+
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsData = new ArrayList<>();
+        LandScape landScape = new LandScape("des","Ảnh đẹp 4k - ảnh nên máy tính");
+        dsData.add(landScape);
+        dsData.add(new LandScape("anh2","Ảnh đẹp 4k - ảnh nên máy tính"));
+        dsData.add(new LandScape("anh3","Ảnh đẹp 4k - ảnh nên máy tính"));
+        dsData.add(new LandScape("anh4","Ảnh đẹp 4k - ảnh nên máy tính"));
+        return dsData;
+    }
+}
