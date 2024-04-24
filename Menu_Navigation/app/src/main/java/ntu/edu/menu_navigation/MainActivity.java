79f25e0bedb45eba267aa5617e190a500a749f45
FOG <dn135897@gmail.com>
2024-04-24 14:46:29 +0700

    finished menu_navigation

---
+++ Menu_Navigation/app/src/main/java/ntu/edu/menu_navigation/MainActivity.java
@@ -0,0 +1,56 @@
+package ntu.edu.menu_navigation;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ImageButton btn_menu;
+    private NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.main);
+        btn_menu = findViewById(R.id.btn_menu);
+        navigationView = findViewById(R.id.navigation_menu);
+
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemID = item.getItemId();
+
+                if(itemID == R.id.btn_close)
+                {
+                    drawerLayout.close();
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
